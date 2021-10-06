5b5ebbd8a7fa8e07389ea8d0978f0122a20d2fa1
DESKTOP-E8R1B6H\quyph <quyphuc211120@gmail.com>
2021-10-06 20:29:54 +0700

    finish

--- index.js
+++ index.js
@@ -3,12 +3,49 @@ const app = express();
 let port = process.env.PORT || 3000;
 const importData = require("./data.json")
 
+
 app.get("/",(req, res) => {
-    res.send("Hello World");
+    res.send(importData);
 })
 app.get("/apple", (req,res) => {
-    res.send(importData)
+    res.send(importData.apple)
 })
+app.get("/samsung", (req,res) => {
+    res.send(importData.samsung)
+})
+app.get("/xiaomi", (req,res) => {
+    res.send(importData.xiaomi)
+})
+app.get("/oppo", (req,res) => {
+    res.send(importData.oppo)
+})
+app.get("/oppo", (req,res) => {
+    res.send(importData.oppo)
+})
+app.get("/realme", (req,res) => {
+    res.send(importData.realme)
+})
+app.get("/nokia", (req,res) => {
+    res.send(importData.nokia)
+})
+app.get("/vsmart", (req,res) => {
+    res.send(importData.vsmart)
+})
+app.get("/vivo", (req,res) => {
+    res.send(importData.vivo)
+})
+app.get("/asus", (req,res) => {
+    res.send(importData.asus)
+})
+app.get("/oneplus", (req,res) => {
+    res.send(importData.oneplus)
+})
+app.get("/nubia", (req,res) => {
+    res.send(importData.nubia)
+})
+
+
 app.listen(port, ()=> {
     console.log(`app is running on port ${port}`)
+    console.log(importData.apple)
 })
