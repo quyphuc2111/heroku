245112545cd91df99ec49b534f7501ecd88e2b31
DESKTOP-E8R1B6H\quyph <quyphuc211120@gmail.com>
2021-10-19 01:31:11 +0700

    make it better

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const importData = require("./data.json")
 const cors = require('cors');
 
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'http://phuc-shop.surge.sh', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
