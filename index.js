b95ddfccdc90dae1128df6388d84221c7fad4f9b
DESKTOP-E8R1B6H\quyph <quyphuc211120@gmail.com>
2021-10-19 20:54:47 +0700

    up lc

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const importData = require("./data.json")
 const cors = require('cors');
 
 const corsOptions ={
-    origin:'http://phuc-shop.surge.sh', 
+    origin:'http://localhost:3000/', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
