2036f56736116e58e476bac03b205e1e5f7c0e93
DESKTOP-E8R1B6H\quyph <quyphuc211120@gmail.com>
2021-12-31 09:38:40 +0700

    last

--- index.js
+++ index.js
@@ -2,21 +2,23 @@ const express = require("express");
 const app = express();
 let port = process.env.PORT || 3000;
 const importData = require("./data.json")
-const cors = require('cors');
+// const cors = require('cors');
 
 
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
-app.use((req,res,next)=>{
-    res.setHeader('Acces-Control-Allow-Origin','*');
-    res.setHeader('Acces-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Acces-Contorl-Allow-Methods','Content-Type','Authorization');
-    next(); 
-})
+// const corsOptions ={
+//     origin:'http://localhost:3000', 
+//     credentials:true,           
+//     optionSuccessStatus:200
+// }
+// app.use(cors(corsOptions));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
+
 
 app.get("/product",(req, res) => {
     res.send(importData);
